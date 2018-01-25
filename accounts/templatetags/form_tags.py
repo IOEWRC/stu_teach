dc8b55bad0e16cececba1e663b938024d345ac76
Pawan <pawanpaudel93@gmail.com>
2018-01-25 17:09:55 +0545

    commit before merging sagar_branch's registration_redux

--- accounts/templatetags/form_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template, forms
-
-
-register = template.Library()
-
-
-@register.filter
-def is_file_field(field):
-    return type(field.field).__name__ == 'ImageField'
--- stu_teach/settings.py
+++ stu_teach/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
+EMAIL_HOST_USER = 'stu.teach.email@gmail.com' # email id
+EMAIL_HOST_PASSWORD = 'stu_teach123456' #password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
