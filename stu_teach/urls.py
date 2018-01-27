4afb893fb64d7b7aa552a259788c4641555f6149
Pawan <pawanpaudel93@gmail.com>
2018-01-27 15:09:13 +0545

    added bootstrap and css for registration_redux and other elements

--- stu_teach/urls.py
+++ stu_teach/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import landing_page
 
 urlpatterns = [
+    path('',landing_page),
     path('admin/', admin.site.urls),
     # path('account/', include('accounts.urls')),
     path('home/', include('forum.urls')),
     path('account/', include('registration_redux.backends.default.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
